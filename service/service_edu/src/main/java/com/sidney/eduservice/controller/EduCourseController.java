c824a513b6f13fa0569dcc9fc3f622e207e21600
chen <cxysid@163.com>
2020-05-18 22:21:08 +0800

    完成添加课程信息模块

---
+++ service/service_edu/src/main/java/com/sidney/eduservice/controller/EduCourseController.java
@@ -0,0 +1,34 @@
+package com.sidney.eduservice.controller;
+
+
+import com.sidney.commonutils.R;
+import com.sidney.eduservice.entity.vo.CourseInfoVo;
+import com.sidney.eduservice.service.EduCourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 课程 前端控制器
+ * </p>
+ *
+ * @author Sidney
+ * @since 2020-05-17
+ */
+@RestController
+@RequestMapping("/eduservice/course")
+@CrossOrigin
+public class EduCourseController {
+
+    @Autowired
+    private EduCourseService courseService;
+
+    //添加课程基本信息的方法
+    @PostMapping("addCourseInfo")
+    public R addCourseInfo(@RequestBody CourseInfoVo courseInfoVo){
+        String id = courseService.saveCourseInfo(courseInfoVo);
+        return R.ok().data("courseId",id);
+    }
+
+}
+
---
+++ service/service_edu/src/main/java/com/sidney/eduservice/controller/EduCourseDescriptionController.java
@@ -0,0 +1,21 @@
+package com.sidney.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程简介 前端控制器
+ * </p>
+ *
+ * @author Sidney
+ * @since 2020-05-17
+ */
+@RestController
+@RequestMapping("/eduservice/edu-course-description")
+public class EduCourseDescriptionController {
+
+}
+
